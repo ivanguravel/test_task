1dab9abe9c7d6eb38c5dc3bfe20f966500e85e2b
Ivan Zhuravel <ivanguravel@gmail.com>
2017-03-23 23:02:59 +0200

    hot fix

--- receiver/src/main/java/com/telco/app/receiver/BridgeMessageReceiver.java
+++ receiver/src/main/java/com/telco/app/receiver/BridgeMessageReceiver.java
@@ -2,7 +2,6 @@ package com.telco.app.receiver;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.telco.app.dto.BridgeDto;
-import com.telco.app.dto.BridgeResponse;
 import com.telco.app.service.BridgeService;
 import com.telco.app.service.CustomObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,16 +21,6 @@ public class BridgeMessageReceiver {
 	public void receiveMessage(final String message) throws IOException {
 		List<BridgeDto> bridges = objectMapper.readValue(message, new TypeReference<List<BridgeDto>>() { });
 		service.save(bridges);
-	}
-
-	private List<BridgeDto> jsonToBridges(final String json) {
-		try {
-			BridgeResponse response = objectMapper.readValue(json, BridgeResponse.class);
-			return response.getBridgeDtos();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
 	}
 }
  
